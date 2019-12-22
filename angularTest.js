f374eef5edf1dd5637af01b885914ddb1bc12306
Amogh M K <amoghmkg@gmail.com>
2019-12-22 19:10:05 +0530

    EMI monthly cost breakup table added

--- angularTest.js
+++ angularTest.js
@@ -14,6 +14,39 @@ function calculatePrincipalAmount (item_cost, interest_rate, duration, no_cost_f
   return item_cost*1;
 }
 
+function create_table_data (principal_amount, interest_rate, emi_amount, duration) {
+  let iter = 0; 
+  let table_data = [];
+  let total_principal_amount = 0, total_interest_amount = 0, total_gst_amount = 0, total_amount = 0;
+  while (iter<duration) {
+    let interest_amount_month = principal_amount * interest_rate;
+    let gst_amount_month = interest_amount_month * 0.18;
+    let principal_amount_month = emi_amount - interest_amount_month;
+    principal_amount = principal_amount - principal_amount_month;
+    let total_amount_month = principal_amount_month + interest_amount_month + gst_amount_month;
+    let table_row = {
+      'month': ++iter,
+      'principal_amount_month': parseFloat(principal_amount_month.toFixed(2)),
+      'interest_amount_month': parseFloat(interest_amount_month.toFixed(2)),
+      'gst_amount_month': parseFloat(gst_amount_month.toFixed(2)),
+      'total_amount_month': parseFloat(total_amount_month.toFixed(2))
+    };
+    total_principal_amount += principal_amount_month;
+    total_interest_amount += interest_amount_month;
+    total_gst_amount += gst_amount_month;
+    total_amount += total_amount_month;
+    table_data.push(table_row);
+  }
+  let table_total_row = {
+    'month': 'Total',
+    'principal_amount_month': parseFloat(total_principal_amount.toFixed(2)),
+    'interest_amount_month': parseFloat(total_interest_amount.toFixed(2)),
+    'gst_amount_month': parseFloat(total_gst_amount.toFixed(2)),
+    'total_amount_month': parseFloat(total_amount.toFixed(2))
+  };
+  table_data.push(table_total_row);
+  return table_data;
+}
 
 var app = angular.module('emi-form', []);
 
@@ -28,12 +61,19 @@ app.controller('emi-form-controller', function($scope) {
       return
     }
     let interest_rate = ($scope.interest_rate)/1200;
-    $scope.emi_amount = parseFloat(calculateEmiAmount($scope.item_cost, interest_rate, $scope.duration, $scope.no_cost_flag).toFixed(2));
-    $scope.principal_amount = parseFloat(calculatePrincipalAmount($scope.item_cost, interest_rate, $scope.duration, $scope.no_cost_flag, $scope.emi_amount).toFixed(2));
-    $scope.interest_amount = parseFloat(($scope.emi_amount*$scope.duration - $scope.principal_amount).toFixed(2));
-    $scope.gst_amount = parseFloat(($scope.interest_amount * 0.18).toFixed(2));
-    $scope.total_amount = parseFloat(($scope.emi_amount*$scope.duration + $scope.gst_amount).toFixed(2));
-    console.log($scope)
+
+    let emi_amount = calculateEmiAmount($scope.item_cost, interest_rate, $scope.duration, $scope.no_cost_flag);
+    let principal_amount = calculatePrincipalAmount($scope.item_cost, interest_rate, $scope.duration, $scope.no_cost_flag, emi_amount);
+    let interest_amount = emi_amount*$scope.duration - principal_amount;
+    let gst_amount = interest_amount * 0.18;
+    let total_amount = emi_amount*$scope.duration + gst_amount;
+
+    $scope.emi_amount = parseFloat(emi_amount.toFixed(2));
+    $scope.principal_amount = parseFloat(principal_amount.toFixed(2));
+    $scope.interest_amount = parseFloat(interest_amount.toFixed(2));
+    $scope.gst_amount = parseFloat(gst_amount.toFixed(2));
+    $scope.total_amount = parseFloat(total_amount.toFixed(2));
+    $scope.table_data = create_table_data(principal_amount, interest_rate, emi_amount, $scope.duration);
     $scope.result_show = true;
   }
 });
